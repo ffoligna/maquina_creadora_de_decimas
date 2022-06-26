c1c6bf5caaf5fef4b537597cb3f14d09e34f980d
Federico Foligna <ffoligna@users.noreply.github.com>
2022-06-26 16:13:04 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from almafuerte import almafuerte_01, almafuerte_02
 from aznar import aznar_01, aznar_02
 from drexler import drexler_01, drexler_02, drexler_03
 from zitarrosa import zitarrosa_01, zitarrosa_02, zitarrosa_03
-
+from menus import menu_principal, acerca_del_proyecto
+from os import system
 
 # Lista de estrofas
 estrofa = [
@@ -22,9 +23,42 @@ estrofa = [
 
 # Función que imprime una linea aleatoria de una estrofa aleatoria
 def maquina_de_decimas():
+    print("""### Décima Aleatoria ###
+    """)
     for i in range(10):
         e = choice(estrofa)
         print(f"{i+1} {e[i]}")
-        sleep(1)
+        sleep(0.5)
+    print("""
+### Volverá al menú principal en 15 segundos ###""")
 
-maquina_de_decimas()
+# Función limpiar pantalla
+def limpiar_pantalla():
+    system("clear")
+
+# Programa principal
+while True:
+    limpiar_pantalla()
+    menu = input(menu_principal)
+
+    while menu not in ["1", "2", "3"]:
+        print(f"""
+########## Error!!! ##########
+Sólo se puede elegir 1, 2 o 3.
+##############################
+""")
+        menu = input(menu_principal)
+
+    if menu == "1":
+        limpiar_pantalla()
+        maquina_de_decimas()
+        sleep(15)
+    elif menu == "2":
+        limpiar_pantalla()
+        for x in acerca_del_proyecto:
+            print(x, end='', flush=True)
+            sleep(0.05)
+        sleep(10)
+    elif menu == "3":
+        break
+    
---
+++ menus.py
@@ -0,0 +1,36 @@
+menu_principal = """
+############ Bienvenido/a ############
+###### Máquina de Crear Décimas ######
+
+########### Menú Principal ###########
+
+Sólo se puede elegir 1, 2 o 3.
+
+1) Crear Décima
+2) Acerca del proyecto
+3) Cerrar programa
+
+######################################
+
+>_ """
+
+acerca_del_proyecto = """
+#############################################################################
+############################ Acerca del Proyecto ############################
+#############################################################################
+
+Juego/Programa que genera estrofas aleatorias, con rima en décimas espinelas,
+basándonos en canciones/poesías que cumplan con la estructura "abbaaccddc".
+
+Para este proyecto, elegimos las siguientes canciones y poesías:
+
+> Alfredo Zitarrosa - 'Diez Décimas de Saludo al Público Argentino'
+
+> Almafuerte (Pedro Bonifacio Palacios) - 'Décimas'
+
+> Jorge Drexler - 'Milonga del Moro Judío'
+
+> Pedro Aznar - 'Décimas'
+
+################## Volverá al menú principal en 10 segundos ##################
+"""
